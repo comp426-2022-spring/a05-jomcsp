fdddb024f7a94db8d1da56afc60216e3d2ba031d
jomcsp <jomcsp@live.unc.edu>
2022-04-28 12:31:12 -0400

    7

--- public/main.mjs
+++ public/main.mjs
@@ -17,7 +17,7 @@ import {flip} from "../src"
         document.getElementById("home").setAttribute("class","active");
         document.getElementById("multi").setAttribute("class","hidden");
         document.getElementById("guess").setAttribute("class","hidden");
-        gitcommit= 6;
+        gitcommit= 7;
     }
 
     function displayMulti(event) {
